96fd0e6a8c0d097016ce2b54cbd305aed381d900
Skatlem <115564126+Skatlem@users.noreply.github.com>
2023-02-14 11:28:58 +0100

    fix

---
+++ ErrorHandle.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "ErrorHandle.h"
+
+ErrorHandle::ErrorHandle(){
+    handler = false;
+}
+
+ErrorHandle::~ErrorHandle(){
+}
+
+void ErrorHandle::setHandler(bool state){
+    handler = state;
+}
